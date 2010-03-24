17cf8750d1983e4676490c3e4d82e8a8ed429674
gabathuler <gabathuler@7a51c424-2464-11df-9033-5798db0ff93d>
2010-03-24 18:24:24 +0000

    defect3 and renaming

--- iphone/trunk/Classes/AddListViewController.m
+++ iphone/trunk/Classes/AddListViewController.m
@@ -1,51 +1,54 @@
 //
-//  AddListViewController.m
+//  ShareListViewController.m
 //  Lightning
 //
-//  Created by Cyril Gabathuler on 08.03.10.
+//  Created by Cyril Gabathuler on 24.02.10.
 //  Copyright 2010 Bahnhofstrasse 24, 5400 Baden. All rights reserved.
 //
 
 #import "AddListViewController.h"
-#import "ShareListViewController.h"
 
 @implementation AddListViewController
 
-
 /*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
- - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
- if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
- // Custom initialization
+ - (id)initWithStyle:(UITableViewStyle)style {
+ // Override initWithStyle: if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+ if (self = [super initWithStyle:style]) {
  }
  return self;
  }
  */
 
-
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-	NSLog(@"AddListViewController");
-	shareListViewController = [[ShareListViewController alloc] init];
-	
-	UITableView *shareListView = [[UITableView alloc]initWithFrame:CGRectMake(100, 30, 100, 200) style:UITableViewStylePlain];
-	
-	shareListView.delegate = shareListViewController;
-	shareListView.dataSource = shareListViewController;
-	
-	
-	[self.navigationController.view addSubview:shareListView];
-	
-}
-
-
-
- // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-/* - (void)viewDidLoad {
- [super viewDidLoad];
-
+/*
+ - (void)viewDidLoad {
+	 [super viewDidLoad];
+	 
+ // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+ // self.navigationItem.rightBarButtonItem = self.editButtonItem;
  }*/
  
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+	self.title = @"Add List";
+}
+
+/*
+ - (void)viewDidAppear:(BOOL)animated {
+ [super viewDidAppear:animated];
+ }
+ */
+/*
+ - (void)viewWillDisappear:(BOOL)animated {
+ [super viewWillDisappear:animated];
+ }
+ */
+/*
+ - (void)viewDidDisappear:(BOOL)animated {
+ [super viewDidDisappear:animated];
+ }
+ */
 
 /*
  // Override to allow orientations other than the default portrait orientation.
@@ -54,24 +57,150 @@
  return (interfaceOrientation == UIInterfaceOrientationPortrait);
  }
  */
-/*
+
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
 	
 	// Release any cached data, images, etc that aren't in use.
 }
-*/
+
 - (void)viewDidUnload {
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
 }
 
 
+#pragma mark Table view methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+	if(section == 0)
+		return @"Adding a new list";
+	else
+		return @"Share list with";
+
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+	
+	if(section == 0) {
+		return tableView.tableHeaderView;
+	} else {
+		UIView *view = [[[UIView alloc] initWithFrame:CGRectMake(10, 10, 100, 80)] autorelease];
+		
+		UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+		button.frame = CGRectMake(10, 30, 300, 40);
+		[button setTitle:@"Add new person" forState:UIControlStateNormal];
+		[view addSubview:button];
+		
+		UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(18, 0, 200, 20)];
+		label.text = @"Share list with";
+		[label setBackgroundColor:tableView.backgroundColor];
+		[view addSubview:label];
+		
+		return view;
+	}
+	
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+	if(section == 0)
+		return 30;
+	else
+		return 80;
+}
+
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if(section == 0)
+		return 1;
+	
+	return 3;
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+    // Set up the cell...
+	if(indexPath.section == 0) {
+		UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(16, 10, cell.frame.size.width-16, cell.frame.size.height-10)];
+		textField.placeholder = @"Set name of list";
+		[cell addSubview: textField];
+		//cell.textLabel.text = @"Set name of list";
+	} else {
+		cell.textLabel.text = @"test";
+	}
+	
+    return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Navigation logic may go here. Create and push another view controller.
+	// AnotherViewController *anotherViewController = [[AnotherViewController alloc] initWithNibName:@"AnotherView" bundle:nil];
+	// [self.navigationController pushViewController:anotherViewController];
+	// [anotherViewController release];
+}
+
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ 
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
+ }   
+ else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }   
+ }
+ */
+
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
+
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+
 - (void)dealloc {
     [super dealloc];
-	[shareListViewController release];
 }
 
 
 @end
+
+
