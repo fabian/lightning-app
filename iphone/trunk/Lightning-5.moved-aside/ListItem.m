39c0634f8ca730f1bc93d2202b7466042ee30915
gabathuler <gabathuler@7a51c424-2464-11df-9033-5798db0ff93d>
2010-10-15 12:00:31 +0000

    list working with data from service

--- iphone/trunk/Lightning-5.moved-aside/ListItem.m
+++ iphone/trunk/Lightning-5.moved-aside/ListItem.m
@@ -2,7 +2,7 @@
 //  ListItem.m
 //  Lightning
 //
-//  Created by Cyril Gabathuler on 15.10.10.
+//  Created by Cyril Gabathuler on 22.09.10.
 //  Copyright 2010 Bahnhofstrasse 24, 5400 Baden. All rights reserved.
 //
 
