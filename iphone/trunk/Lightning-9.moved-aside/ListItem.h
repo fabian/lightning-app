3ebf0818e37892eefe0291e413c0abc6d8395f97
gabathuler <gabathuler@7a51c424-2464-11df-9033-5798db0ff93d>
2011-01-11 11:53:13 +0000

    changes

---
+++ iphone/trunk/Lightning-9.moved-aside/ListItem.h
@@ -0,0 +1,23 @@
+//
+//  ListItem.h
+//  Lightning
+//
+//  Created by Cyril Gabathuler on 18.10.10.
+//  Copyright 2010 Bahnhofstrasse 24, 5400 Baden. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+
+@interface ListItem :  NSManagedObject  
+{
+}
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * creation;
+@property (nonatomic, retain) NSNumber * listItemId;
+
+@end
+
+
+
