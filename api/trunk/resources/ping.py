ef3d848caaa063abddd219bcaa7f21c30bc4117f
Fabian Vogler <fabian@equivalence.ch>
2011-02-08 16:01:45 +0000

    Remove unneeded imports

--- api/trunk/resources/ping.py
+++ api/trunk/resources/ping.py
@@ -1,12 +1,4 @@
-import os
-import logging
-import binascii
-import urbanairship
-import settings
-from util import Resource, json, device_required
-from models import Device, List
-from notifications import Unread
-from resources.list import ListsResource
+from util import Resource, json
 
 class PingResource(Resource):
     
