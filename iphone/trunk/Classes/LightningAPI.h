af9da9c7f21223aa4fa7d75df383f606221ad098
gabathuler <gabathuler@7a51c424-2464-11df-9033-5798db0ff93d>
2011-09-05 16:54:40 +0000

    new api file

--- iphone/trunk/Classes/LightningAPI.h
+++ iphone/trunk/Classes/LightningAPI.h
@@ -12,6 +12,7 @@
 
 @protocol LightningAPIListsDelegate;
 @protocol LightningAPIAddListDelegate;
+@protocol LightningAPIReadListDelegate;
 
 @interface LightningAPI : NSObject
 
@@ -25,17 +26,20 @@
 @property (nonatomic, retain) NSURL *apiURL;
 @property (assign) id <LightningAPIListsDelegate> delegate;
 @property (assign) id <LightningAPIAddListDelegate> addListDelegate;
+@property (assign) id <LightningAPIReadListDelegate> readListDelegate;
 
 + (LightningAPI*) sharedManager;
 
 - (void)setupDevice;
 - (void)initLightningWithContext:(NSManagedObjectContext *)context deviceToken:(NSString *)deviceToken;
 - (void)prepareRequest:(NSMutableURLRequest *) request device:(Boolean)device;
+- (void)updateDevice:(NSString *)updatedDeviceToken Name:(NSString *)updatedName;
 - (void)getLists;
 - (void)addList:(NSString *)listTitle;
 - (void)updateItem:(ListItem *)listItem;
 - (void)addItemToList:(NSNumber *)listId item:(ListItem *)item;
 - (void)pushUpdateForList:(NSNumber *)listId;
+- (void)readList:(NSString *)listId;
     
 @end
 
@@ -48,6 +52,12 @@
 @protocol LightningAPIAddListDelegate <NSObject>
 
 - (void)finishAddList:(NSString *)token;
+
+@end
+
+@protocol LightningAPIReadListDelegate <NSObject>
+
+- (void)finishReadList;
 
 @end
 
