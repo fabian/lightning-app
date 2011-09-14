aba3348a0b36b1619021bb0370c62d9864950cbe
Fabian Vogler <fabian@equivalence.ch>
2011-09-14 21:04:13 +0000

    Don't mock datetime, GAE doesn't like it

--- api/trunk/tests/util.py
+++ api/trunk/tests/util.py
@@ -20,8 +20,3 @@ class Tests(mocker.MockerTestCase):
         self.mocker.count(1, None)
         self.urbanairship = self.mocker.mock()
         self.mocker.result(self.urbanairship)
-
-    def mock_datetime(self, time=datetime(2010, 06, 29, 13, 00, 00)):
-        datetime = self.mocker.replace('datetime.datetime')
-        datetime.now()
-        self.mocker.result(time)
