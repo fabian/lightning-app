d82ea0d627858b7a2167f0422259df7f8bdd8b19
fabian.vogler <fabian.vogler@7a51c424-2464-11df-9033-5798db0ff93d>
2011-09-19 17:52:57 +0000

    Marvin the Paranoid Android

---
+++ api/trunk/marvin.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import urllib
+import urllib2
+import httplib
+import json
+
+url = 'http://localhost:8080'
+#url = 'https://lightning-app.appspot.com'
+
+# create device Marvin and read id
+req = urllib2.Request(url + '/api/devices', 'name=Marvin&identifier=101010&device_token=FE66489F304DC75B8D6E8200DFF8A456E8DAEACEC428B427E9518741C92C6660', {'Environment': 'development'})
+result = urllib2.urlopen(req)
+device = json.loads(result.read())
+
+marvin_id = device['id']
+marvin_url = device['url']
+
+print 'Added Marvin with ID %d' % marvin_id
+
+
+# create device Zem and read id
+req = urllib2.Request(url + '/api/devices', 'name=Zem&identifier=010101&device_token=FE66489F304DC75B8D6E8200DFF8A456E8DAEACEC428B427E9518741C92C6660', {'Environment': 'development'})
+result = urllib2.urlopen(req)
+device = json.loads(result.read())
+
+zem_id = device['id']
+zem_url = device['url']
+
+print 'Added Zem with ID %d' % zem_id
+
+
+# create list
+req = urllib2.Request(url + '/api/lists', 'title=Groceries&owner=%d' % marvin_id, {'Device': marvin_url, 'Environment': 'development'})
+result = urllib2.urlopen(req)
+list = json.loads(result.read())
+
+list_id = list['id']
+list_token = list['token']
+
+print 'Created list with ID %d' % list_id
+
+
+# add Zem to list
+req = urllib2.Request(url + '/api/devices/%d/lists/%d' % (zem_id, list_id), 'token=' + list_token, {'Device': zem_url, 'Environment': 'development'})
+req.get_method = lambda: 'PUT'
+result = urllib2.urlopen(req)
+
+print 'Added Zem to list'
+
+
+# add item to list
+req = urllib2.Request(url + '/api/items', 'value=Milk&list=%d' % list_id, {'Device': marvin_url, 'Environment': 'development'})
+urllib2.urlopen(req)
+
+print 'Added Milk to list'
+
+
+# push
+req = urllib2.Request(url + '/api/lists/%d/devices/%d/push' % (list_id, marvin_id), '', {'Device': marvin_url, 'Environment': 'development'})
+result = urllib2.urlopen(req)
+print result.read()
+
+print 'Pushed'
+
