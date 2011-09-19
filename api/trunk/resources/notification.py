764167b7e3e3fe084bb9b1b9b44919e692d840d9
Fabian Vogler <fabian@equivalence.ch>
2011-09-19 19:14:04 +0000

    Fix push

--- api/trunk/resources/notification.py
+++ api/trunk/resources/notification.py
@@ -70,57 +70,67 @@ class ListPushResource(ListsResource):
             if self.has_access(list):
                 
                 try:
-                    exclude = Device.get_by_id(int(device_id))
+                    device = Device.get_by_id(int(device_id))
                 except ValueError:
-                    exclude = False
+                    device = False
                 
-                if exclude:
+                if device:
                     
-                    devices = []
-                    airship = urbanairship.Airship(self.settings.URBANAIRSHIP_APPLICATION_KEY, self.settings.URBANAIRSHIP_MASTER_SECRET)
-                    
-                    log = list.get_log(list.pushed)
-                    
-                    notification = Notification(log)
-                    message = notification.get_message()
-                    
-                    for x in list.listdevice_set.filter('device != ', exclude):
-                        if x.device.device_token:
-                            
-                            # unread count
-                            device = x.device
-                            count = 0
-                            for y in device.listdevice_set.filter('deleted != ', True):
-                                if y.list.modified > y.read:
-                                    count += 1
-                            
-                            payload = {'badge': count}
-                            if message:
-                                payload['alert'] = message
-                                payload['lightning_list'] = list.key().id()
-                            
-                            try:
-                                # push notification and unread count to Urban Airship
-                                airship.push({'aps': payload}, device_tokens=[x.device.device_token])
+                    # device must match authenticated device
+                    if device.key() == self.get_auth().key():
+                        
+                        devicelist = list.has_access(device)
+                        
+                        devices = []
+                        airship = urbanairship.Airship(self.settings.URBANAIRSHIP_APPLICATION_KEY, self.settings.URBANAIRSHIP_MASTER_SECRET)
+                        
+                        log = list.get_log(device, devicelist.pushed)
+                        
+                        notification = Notification(log)
+                        message = notification.get_message()
+                        
+                        for x in list.listdevice_set.filter('device != ', device):
+                            if x.device.device_token:
                                 
-                                logging.debug("Pushed '%s' (%s) to device %s with device token '%s'.", message, count, x.device.key().id(), x.device.device_token)
+                                # unread count
+                                count = 0
+                                for y in x.device.listdevice_set.filter('deleted != ', True):
+                                    if y.list.modified > y.read:
+                                        count += 1
                                 
-                                devices.append(x.device)
-                            
-                            except DownloadError, e:
-                                logging.error("Unable to push '%s' (%s) to device %s with device token '%s' at Urban Airship: %s", message, count, x.device.key().id(), x.device.device_token, e)
-                            
-                            except urbanairship.AirshipFailure, (status, response):
-                                logging.error("Unable to push '%s' (%s) to device %s with device token '%s' at Urban Airship: %s (%d)", message, count, x.device.key().id(), x.device.device_token, response, status)
+                                payload = {'badge': count}
+                                if message:
+                                    payload['alert'] = message
+                                    payload['lightning_list'] = list.key().id()
+                                
+                                try:
+                                    # push notification and unread count to Urban Airship
+                                    airship.push({'aps': payload}, device_tokens=[x.device.device_token])
+                                    
+                                    logging.debug("Pushed '%s' (%s) to device %s with device token '%s'.", message, count, x.device.key().id(), x.device.device_token)
+                                    
+                                    devices.append(x.device)
+                                
+                                except DownloadError, e:
+                                    logging.error("Unable to push '%s' (%s) to device %s with device token '%s' at Urban Airship: %s", message, count, x.device.key().id(), x.device.device_token, e)
+                                
+                                except urbanairship.AirshipFailure, (status, response):
+                                    logging.error("Unable to push '%s' (%s) to device %s with device token '%s' at Urban Airship: %s (%d)", message, count, x.device.key().id(), x.device.device_token, response, status)
+                        
+                        devicelist.pushed = datetime.now()
+                        devicelist.put()
+                        
+                        return {'devices': [device.key().id() for device in devices]}
                     
-                    list.pushed = datetime.now()
-                    
-                    return {'devices': [device.key().id() for device in devices]}
-                    
+                    else:
+                        # device does not match authenticated device
+                        self.error(403)
+                        self.response.out.write("Device %s doesn't match authenticated device %s" % (device.key().id(), self.get_auth().key().id()))
+                
                 else:
-                    # device to exclude not found
-                    self.error(400)
-                    self.response.out.write("Device to exclude %s not found" % device_id)
+                    # device to push not found
+                    self.error(404)
+                    self.response.out.write("Device to push %s not found" % device_id)
             
         else:
             # list not found
